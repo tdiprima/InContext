9c428771b07f719338b98987398c37fa098af44b
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-04 08:49:03 -0400

    data

--- src/exitstack_example1.py
+++ src/exitstack_example1.py
@@ -8,7 +8,7 @@ from contextlib import ExitStack
 
 from rich import print
 
-files = ["a.txt", "b.txt", "c.txt"]
+files = ["../data/a.txt", "../data/b.txt", "../data/c.txt"]
 with ExitStack() as stack:
     handles = [stack.enter_context(open(f)) for f in files]
     data = [h.read() for h in handles]
